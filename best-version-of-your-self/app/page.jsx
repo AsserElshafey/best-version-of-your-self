794428759e0972eda44450cb59de5c6ad8408f7e
Asser Elshafey <elshafeyasser@yahoo.com>
2024-05-14 18:39:08 +0300

    fix(page): adjusting home page css for better view

--- best-version-of-your-self/app/page.jsx
+++ best-version-of-your-self/app/page.jsx
@@ -6,21 +6,12 @@ import Login from "@/components/Login";
 
 const Home = () => {
   return (
-    <div className="flex p-10">
-      <div className="lg:flex w-full hidden justify-center gap-40">
-        <div className="ml ml-40">
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
-        </div>
-        <div className="">
-          <Login />
-        </div>
-      </div>
-      <div className="lg:hidden flex w-full justify-center">
+    <div className="flex justify-center md:w-4/6 p-2 md:p-0">
+      <div className="">
         <HabitCard />
+      </div>
+      <div className="hidden md:block fixed right-1 md:w-2/6">
+        <Login />
       </div>
     </div>
   )
