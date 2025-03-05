eeb8e7e5496fb7a6c4c750c7f50e38c4bf0932c6
Loay Waleed <loaywaleed9@gmail.com>
2025-03-05 19:38:51 +0200

    feat: add Umami analytics script to layout and correct website description

--- best-version-of-your-self/app/layout.jsx
+++ best-version-of-your-self/app/layout.jsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 import { AuthProvider } from "../contexts/AuthContext";
 import { Inter, Pacifico } from "next/font/google";
+import Script from "next/script";
 
 // Load Google Fonts
 const inter = Inter({
@@ -14,12 +15,24 @@ const pacifico = Pacifico({ subsets: ["latin"], weight: "400" });
 export const metadata = {
   title: "Better",
   description:
-    "This Website is designed to make you the greatest version of your self",
+    "This Website is designed to make you the greatest version of yourself",
 };
 
 const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="350cae94-af0d-4764-ac52-1cbe297332f8"
+        ></script>{" "}
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="350cae94-af0d-4764-ac52-1cbe297332f8"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${inter.className} bg-gray-100`}>
         <AuthProvider>
           <main>{children}</main>
